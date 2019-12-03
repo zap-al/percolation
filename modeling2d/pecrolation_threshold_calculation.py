ff811b6f3425bd9774323799871a363db8852a79
mrmrmr <aliex.s@yandex.by>
2019-12-03 13:21:56 +0300

    add statistic

--- modeling2d/pecrolation_threshold_calculation.py
+++ modeling2d/pecrolation_threshold_calculation.py
@@ -1,3 +1,5 @@
+import time
+
 from numpy import linspace
 
 from modeling2d.calculation import split_on_clusters, get_grid, NOT_CHECKED, drop_cluster_lower_than
@@ -6,8 +8,8 @@ import matplotlib.pyplot as plt
 
 
 def plot_of_greatest_cluster_per_probability():
-    grid_size = 40
-    repeat_count = 1000
+    grid_size = 100
+    repeat_count = 10000
 
     # 3 -> 0.0, 0.5, 1.0   |   6 -> 0.0, 0.2, 0.4, 0.6, 0.8, 1.0
     probability_step_count = 101
@@ -15,6 +17,7 @@ def plot_of_greatest_cluster_per_probability():
     probability_arr = linspace(0, 1, probability_step_count)
 
     average_per_probability = []
+    start = time.time()
     for probability in probability_arr:
         sum_max_len = 0
         for i in range(repeat_count):
@@ -26,14 +29,34 @@ def plot_of_greatest_cluster_per_probability():
                 if len(value) > max_len:
                     max_len = len(value)
 
-            sum_max_len += max_len
+            if i % 1000 == 0:
+                now = time.time()
+                print(f"probability: {probability}\n repeat_count: {i}\n  time: {now - start} \n")
 
+            sum_max_len += max_len
+        print()
         avg = sum_max_len / repeat_count
         average_per_probability.append(avg)
-    mid = sum(average_per_probability) / len(average_per_probability)
-    # plt.axhline(y=mid, color='r', linestyle='-')
+
+    finish = time.time()
+    print(f"\ntotal time: {finish - start}")
+
+    max_derivative = 0
+    max_derivative_num = 0
+    xStep = 1 / (probability_step_count - 1)
+
+    for i in range(0, len(average_per_probability) - 3):
+        if average_per_probability[i + 2] - average_per_probability[i] > max_derivative:
+            max_derivative_num = i + 1
+            max_derivative = average_per_probability[i + 2] - average_per_probability[i]
+
+    print(f"max_der: {(xStep * max_derivative_num)}")
+    plt.axvline(x=(xStep * max_derivative_num), color='r', linestyle='-')
     plt.plot(probability_arr, average_per_probability)
 
 
+start = time.time()
 plot_of_greatest_cluster_per_probability()
+end = time.time()
+print("time spent: " + str(end - start))
 plt.show()
